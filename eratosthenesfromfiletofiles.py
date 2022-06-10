8182c95804c77f2c229f1666cc4b7b985381b295
codeiosaur <93013950+codeiosaur@users.noreply.github.com>
2022-06-09 19:20:49 -0700

    Added anti-user error code and deprecation warnings.

--- eratosthenesfromfiletofiles.py
+++ eratosthenesfromfiletofiles.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 def eratosthenes(num:int,file:str) -> list:
+    raise Warning('This is deprecated! Use eratosthenesbool instead where possible!')
     prime_attempts = []
     primes = []
     for i in range(2,num+1):
@@ -14,6 +15,12 @@ def eratosthenes(num:int,file:str) -> list:
     return (primes)
 
 def eratosthenesbool(num:int,file: Union[str,list],debug:bool) -> list:
+    assert type(num) == int, "Error: data type of num must be int!"; assert type(debug) == bool, "Error: data type of debug must be bool!"
+    if type(file) == list:
+        if all(type(i) is str for i in lst) == False:
+            raise TypeError("Error: all filenames in list file must be type str!")
+    else:
+        assert type(file) == str, "Error: data type of file must be str/list!"
     if debug == True:
         import time; start_time = time.time()
     primes = [True] * (num+1); primes[0] = False; primes[1] = False
@@ -21,7 +28,7 @@ def eratosthenesbool(num:int,file: Union[str,list],debug:bool) -> list:
         if primes[i] == True:
             for n in range(i**2,num+1,i):
                 primes[n] = False
-    if type(file) == 'str':
+    if type(file) == str:
         with open(file, 'w+') as f:
             for i in range(len(primes)):
                 if primes[i] == True:
@@ -50,12 +57,16 @@ def eratosthenesbool(num:int,file: Union[str,list],debug:bool) -> list:
 
 def find_prime(num:int,length:int) -> int:
     try:
-        list = eratosthenesbool(length,'test.txt')
+        if isinstance(num, int) == False or isinstance(length, int) == False:
+            raise TypeError("One of the passed parameters is not of type 'int'!")
+        list = eratosthenesbool(length,'test.txt',False)
         return list[num]
-    except IndexError:
-        raise Exception('The prime you were accessing does not appear in the first ' + str(length) + ' #s. Please increase the length parameter.')
+    except IndexError as e:
+        raise IndexError('The prime you were accessing does not appear in the first ' + str(length) + ' #s. Please increase the length parameter.') from e
 
 def isPrime(num) -> bool:
+    if not isinstance(num, int):
+        raise TypeError("num should be of type 'int'!")
     for i in range(2,num):
         if num%i == 0:
             return False
