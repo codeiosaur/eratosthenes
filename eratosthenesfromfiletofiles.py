2ca207c4071541b2f51438b919988475f7b72768
codeiosaur <93013950+codeiosaur@users.noreply.github.com>
2022-07-13 21:00:17 -0700

    Add files via upload
    - Changed naming for exceptions raised by incorrect data types.
    - Added a new function, dderatosthenes.

--- eratosthenesfromfiletofiles.py
+++ eratosthenesfromfiletofiles.py
@@ -1,4 +1,5 @@
 from typing import Union
+import time
 
 def eratosthenes(num:int,file:str) -> list:
     prime_attempts = []
@@ -14,7 +15,7 @@ def eratosthenes(num:int,file:str) -> list:
     return (primes)
 
 def eratosthenes_bool(num:int,file: Union[str,list],debug:bool) -> list:
-    assert type(num) == int, "Error: data type of num must be int!"; assert type(debug) == bool, "Error: data type of debug must be bool!"
+    assert type(num) == int, f"Error: expected data type int for num, type {type(num)} given"; assert type(debug) == bool, f"Error: expected data type bool for debug, type {type(debug)} given"
     if type(file) == list:
         if all(type(i) is str for i in file) == False:
             raise TypeError("Error: all filenames in list file must be type str!")
@@ -61,8 +62,8 @@ def eratosthenes_bitflip(num:int,file: list[str],debug:bool) -> list:
     import bitarray
     # lines 64-69: anti-user error catching code
     # lines 70-74: actual sieve
-    assert type(num) == int, "Error: data type of num must be int!"; assert type(debug) == bool, "Error: data type of debug must be bool!"
-    assert type(file) == list, "Error: data type of file must be list!"
+    assert type(num) == int, f"Error: expected data type int for num, type {type(num)} given"; assert type(debug) == bool, f"Error: expected data type bool for debug, type {type(debug)} given"
+    assert type(file) == list, f"Error: expected data type list for file, type {type(file)} given"
     if all(type(i) is str for i in file) == False: # Checks if even a single file name is not a string
         raise TypeError("Error: all filenames in list file must be data type str!")
     if debug == True:
@@ -88,6 +89,37 @@ def eratosthenes_bitflip(num:int,file: list[str],debug:bool) -> list:
                 length_of_file = len(f.readlines()) # NOTE: Only includes prime numbers. Ignores debug options
             with open(current_file, 'a+') as f:
                 exec_time = time.time() - start_time; f.write('# of primes: ' + str(length_of_file) + '\n'); f.write('Execution time: ' + str(exec_time))
+    return (primes)
+
+def dderatosthenes(start:int, num:int,inputfile:list[str], outputfile:str) -> list: # a list of output files is not required 
+    # Optimize Lines 108-115 
+    # Add in-sieve sieving?
+    import bitarray
+    assert type(start) == int, f"Error: expected data type int for start, type {type(start)} given"; assert type(num) == int, f"Error: expected data type int for num, type {type(num)} given"
+    assert type(inputfile) == list, f"Error: expected data type list for inputfile, type {type(inputfile)} given"; assert type(outputfile) == str, f"Error: expected data type str for outputfile, type {type(outputfile)} given"
+    for name in range(len(inputfile)):
+        if isinstance(inputfile[name], str) == False:
+            raise TypeError(f"Error: expected data type str for all inputfile elements, type {type(inputfile[name])} given for element {name} of inputfile")
+    with open(inputfile[-1],'r') as f:
+        for line in f:
+            pass
+        very_last_input_num = line # last line of last file
+
+    primes = bitarray.bitarray(1 for i in range(num-start)) # initialize prime number array
+
+    for i in range(len(inputfile)):
+        curr_i_file = inputfile[i] # current input file
+        with open(curr_i_file, 'r') as f:
+            for line in f:
+                content = int(line.rstrip()) # Sanitize the line to remove \n
+                for j in range(len(primes)): # NOTE: Write input number to sieve of eratosthenes. Optimize further
+                    if (j+start) % content == 0:
+                        primes[j] = 0
+
+    with open(outputfile, 'w+') as f:
+        for i in range(len(primes)):
+            if primes[i] == 1:
+                f.write(str(i+start) + '\n')
     return (primes)
 
 def find_prime(num:int,length:int) -> int:
