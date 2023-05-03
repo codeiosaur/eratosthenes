a690765c4f85fb71fed71fc1ccfddcbb4036667d
codeiosaur <codeiosaur@outlook.com>
2023-05-02 23:11:04 -0700

    Shortened error messages, introduced compliance with PEP 8

--- eratosthenesfromfiletofiles.py
+++ eratosthenesfromfiletofiles.py
@@ -15,12 +15,15 @@ def eratosthenes(num:int,file:str) -> list:
     return (primes)
 
 def eratosthenes_bool(num:int,file: Union[str,list],debug:bool) -> list:
-    assert type(num) == int, f"Error: expected data type int for num, type {type(num)} given"; assert type(debug) == bool, f"Error: expected data type bool for debug, type {type(debug)} given"
+    assert type(num) == int, f"Error: expected type int for num, \
+        type {type(num)} given"
+    assert type(debug) == bool, f"Error: expected type bool for debug, \
+        type {type(debug)} given"
     if type(file) == list:
         if all(type(i) is str for i in file) == False:
-            raise TypeError("Error: all filenames in list file must be type str!")
+            raise TypeError("Error: all filenames in list must be type str!")
     else:
-        assert type(file) == str, "Error: data type of file must be str/list!"
+        assert type(file) == str, "Error: file type must be str/list!"
     if debug == True:
         import time; start_time = time.time()
     primes = [True] * (num+1); primes[0] = False; primes[1] = False
@@ -35,9 +38,12 @@ def eratosthenes_bool(num:int,file: Union[str,list],debug:bool) -> list:
                     f.write(str(i) + '\n')
         if debug == True:
             with open(file, 'r') as f:
-                length_of_file = len(f.readlines()) # NOTE: Only includes prime numbers. Ignores debug options
+                # NOTE: Only includes prime numbers. Ignores debug options
+                length_of_file = len(f.readlines())
             with open(file, 'a+') as f:
-                exec_time = time.time() - start_time; f.write('# of primes: ' + str(length_of_file) + '\n'); f.write('Execution time: ' + str(exec_time))
+                exec_time = time.time() - start_time
+                f.write('# of primes: ' + str(length_of_file) + '\n')
+                f.write('Execution time: ' + str(exec_time))
         return (primes)
     else:
         for i in range(len(file)):
@@ -48,27 +54,37 @@ def eratosthenes_bool(num:int,file: Union[str,list],debug:bool) -> list:
                         if primes[n] == True:
                             f.write(str(n) + '\n')
                 else:
-                    for m in range(((len(primes)//len(file))*i),((len(primes)//len(file))*(i+1))):
+                    for m in range(((len(primes)//len(file))*i),
+                                   ((len(primes)//len(file))*(i+1))):
                         if primes[m] == True:
                             f.write(str(m) + '\n')
             if debug == True:
                 with open(current_file, 'r') as f:
-                    length_of_file = len(f.readlines()) # NOTE: Only includes prime numbers. Ignores debug options
+                    # NOTE: Only includes prime numbers. Ignores debug options
+                    length_of_file = len(f.readlines()) 
                 with open(current_file, 'a+') as f:
-                    exec_time = time.time() - start_time; f.write('# of primes: ' + str(length_of_file) + '\n'); f.write('Execution time: ' + str(exec_time))
+                    exec_time = time.time() - start_time 
+                    f.write('# of primes: ' + str(length_of_file) + '\n')
+                    f.write('Execution time: ' + str(exec_time))
         return (primes)
 
 def eratosthenes_bitflip(num:int,file: list[str],debug:bool) -> list:
     import bitarray
     # lines 64-69: anti-user error catching code
     # lines 70-74: actual sieve
-    assert type(num) == int, f"Error: expected data type int for num, type {type(num)} given"; assert type(debug) == bool, f"Error: expected data type bool for debug, type {type(debug)} given"
-    assert type(file) == list, f"Error: expected data type list for file, type {type(file)} given"
-    if all(type(i) is str for i in file) == False: # Checks if even a single file name is not a string
-        raise TypeError("Error: all filenames in list file must be data type str!")
+    assert type(num) == int, f"Error: expected type int for num, \
+        type {type(num)} given"
+    assert type(debug) == bool, f"Error: expected type bool for debug, \
+        type {type(debug)} given"
+    assert type(file) == list, f"Error: expected type list for file, \
+        type {type(file)} given"
+    # Checks if even a single file name is not a string
+    if all(type(i) is str for i in file) == False:
+        raise TypeError("Error: all filenames in list must be data type str!")
     if debug == True:
         import time; start_time = time.time()
-    primes = bitarray.bitarray(num+1); primes.setall(1); primes[0:2] = 0 # initialize prime number array
+    primes = bitarray.bitarray(num+1)
+    primes.setall(1); primes[0:2] = 0 # initialize prime number array
     for i in range(2,num+1):
         if primes[i] == 1:
             for n in range(i**2,num+1,i):
@@ -81,60 +97,76 @@ def eratosthenes_bitflip(num:int,file: list[str],debug:bool) -> list:
                     if primes[n] == 1:
                         f.write(str(n) + '\n')
             else:
-                for m in range(((len(primes)//len(file))*i),((len(primes)//len(file))*(i+1))):
+                for m in range(((len(primes)//len(file))*i),
+                               ((len(primes)//len(file))*(i+1))):
                     if primes[m] == 1:
                         f.write(str(m) + '\n')
         if debug == True:
             with open(current_file, 'r') as f:
-                length_of_file = len(f.readlines()) # NOTE: Only includes prime numbers. Ignores debug options
+                # NOTE: Only includes prime numbers. Ignores debug options
+                length_of_file = len(f.readlines())
             with open(current_file, 'a+') as f:
-                exec_time = time.time() - start_time; f.write('# of primes: ' + str(length_of_file) + '\n'); f.write('Execution time: ' + str(exec_time))
+                exec_time = time.time() - start_time
+                f.write('# of primes: ' + str(length_of_file) + '\n')
+                f.write('Execution time: ' + str(exec_time))
     return (primes)
 
-def dd_eratosthenes(start:int, num:int,inputfile:list[str], outputfile:str) -> list: # a list of output files is not required 
+def dd_eratosthenes(start:int,num:int,infile:list[str],outfile:str) -> list: 
+    # a list of output files is not required 
     # Optimize Lines 107-113 and 117-124
     # Add in-sieve sieving?
     import bitarray
-    assert type(start) == int, f"Error: expected data type int for start, type {type(start)} given"; assert type(num) == int, f"Error: expected data type int for num, type {type(num)} given"
-    assert type(inputfile) == list, f"Error: expected data type list for inputfile, type {type(inputfile)} given"; assert type(outputfile) == str, f"Error: expected data type str for outputfile, type {type(outputfile)} given"
-    # FIXME: Fix an edge case where if start is sufficiently small and num is sufficiently large, some numbers are mistakenly identified as prime when they are actually composite. Seems to happen when start**2 < num.
-    # An assertion has been added to stop people from making the error until a fix has been found. Currently trying to fix in lines 127 through 136.
+    assert type(start) == int, f"Error: expected type int for start, \
+        type {type(start)} given"
+    assert type(num) == int, f"Error: expected type int for num, \
+        type {type(num)} given"
+    assert type(infile) == list, f"Error: expected type list for infile, \
+        type {type(infile)} given"
+    assert type(outfile) == str, f"Error: expected type str for outfile, \
+        type {type(outfile)} given"
+    # FIXME: Fix an edge case where if start**2 < num, 
+    # Some numbers are misidentified as prime when they are actually composite. 
+    # An assertion has been added to prevent this until a fix has been found.
     try:
         assert start**2 > num
     except AssertionError:
-        raise NotImplementedError(f'A bug fix for number ranges such as {start**2} through {num} is in progress! This is a placeholder error until a fix is found.')
+        raise NotImplementedError(f'A bug for number ranges like {start**2} \
+            through {num} exists! Please select another range instead.')
 
-    for name in range(len(inputfile)):
-        if isinstance(inputfile[name], str) == False:
-            raise TypeError(f"Error: expected data type str for all inputfile elements, type {type(inputfile[name])} given for element {name} of inputfile")
-    with open(inputfile[-1],'r') as f:
+    for name in range(len(infile)):
+        if isinstance(infile[name], str) == False:
+            raise TypeError(f"Error: expected type str for all infile elements"
+                            f", type {type(infile[name])}"
+                            f" given for element {name} of infile")
+    with open(infile[-1],'r') as f:
         for line in f:
             pass
         very_last_input_num = line # last line of last file
+    # initialize prime number array
+    primes = bitarray.bitarray(1 for i in range(num-start))
 
-    primes = bitarray.bitarray(1 for i in range(num-start)) # initialize prime number array
-
-    for i in range(len(inputfile)):
-        curr_i_file = inputfile[i] # current input file
+    for i in range(len(infile)):
+        curr_i_file = infile[i] # current input file
         with open(curr_i_file, 'r') as f:
             for line in f:
                 content = int(line.rstrip()) # Sanitize the line to remove \n
-                for j in range(len(primes)): # NOTE: Write input number to sieve of eratosthenes. Optimize further
+                for j in range(len(primes)):
+                    # NOTE: Write input # to sieve of eratosthenes. Optimize
                     if (j+start) % content == 0:
                         primes[j] = 0
 
     # FIXME: See line 100
-    # if (start**2 <= num): # If start**2 < num, run normal sieve in addition to reading from file.
+    # if (start**2 <= num): #If start**2 < num, run normal sieve and file read.
     #     for i in range(len(primes)):
     #         if primes[i] == 1:
     #             if i == 0:
     #                 for n in range((start)**2, num+1):
-    #                     pass # Implement edge case if the first number in the bitarray happens to be prime
+    #                     pass # Implement edge case if 1st # is prime
     #             else:
     #                 for n in range((i+start)**2,num+1,i):
     #                     primes[(n-start)] = 0 # TEST THIS
 
-    with open(outputfile, 'w+') as f:
+    with open(outfile, 'w+') as f:
         for i in range(len(primes)):
             if primes[i] == 1:
                 f.write(str(i+start) + '\n')
@@ -143,11 +175,13 @@ def dd_eratosthenes(start:int, num:int,inputfile:list[str], outputfile:str) -> l
 def find_prime(num:int,length:int) -> int:
     try:
         if isinstance(num, int) == False or isinstance(length, int) == False:
-            raise TypeError("One of the passed parameters is not of type 'int'!")
+            raise TypeError("A passed parameter is not of type 'int'!")
         list = eratosthenes_bitflip(length,'test.txt',False)
         return list[num]
     except IndexError as e:
-        raise IndexError('The prime you were accessing does not appear in the first ' + str(length) + ' #s. Please increase the length parameter.') from e
+        raise IndexError('The prime you were accessing does not appear in the' 
+                         'first ', + str(length) + 
+                         ' #s. Please increase the length parameter.') from e
 
 def is_prime(num) -> bool:
     if not isinstance(num, int):
@@ -160,17 +194,31 @@ def is_prime(num) -> bool:
 def file_name_gen(folder:str, end:int, file_num:int) -> list:
     file_list = []; sep = end/file_num
     for i in range(1,file_num+1):
-        num_1 = str("{:e}".format((i-1)*sep)); num_2 = str("{:e}".format(i*sep)) # Get scientific numbers, exponent and all!
-        num_1 = num_1.split('e'); num_1_exp = 'e' + num_1[1]; num_1_dec = num_1[0] # split into two parts: the exponenent and the power, called num_1_exp and num_1_dec respectively.
-        num_2 = num_2.split('e'); num_2_exp = 'e' + num_2[1]; num_2_dec = num_2[0]
-        num_1_dec = num_1_dec.rstrip("0"); num_2_dec = num_2_dec.rstrip("0") # remove all trailing zeroes from number
-        
-        if num_1_dec[-1] == '.': # adds trailing zeroes to avoid rogue trailing decimal points
+        # Get scientific numbers, exponent and all
+        num_1 = str("{:e}".format((i-1)*sep))
+        num_2 = str("{:e}".format(i*sep))
+
+        num_1 = num_1.split('e')
+        num_1_exp = 'e' + num_1[1] # exponent part of num
+        num_1_dec = num_1[0] # decimal part of num
+
+        num_2 = num_2.split('e')
+        num_2_exp = 'e' + num_2[1]
+        num_2_dec = num_2[0]
+        # remove all trailing zeroes to prevent rogue decimal points
+        num_1_dec = num_1_dec.rstrip("0")
+        num_2_dec = num_2_dec.rstrip("0")
+        # adds trailing zeroes to avoid rogue trailing decimal points
+        if num_1_dec[-1] == '.':
             num_1_dec += '0'
         if num_2_dec[-1] == '.':
             num_2_dec += '0'
-        file_name = f"{folder}/primes-{num_1_dec}{num_1_exp}-{num_2_dec}{num_2_exp}.txt"; file_list.append(file_name) # file name formatting
-        f = open(file_name,'w+'); f.close() # the file creation bit; creates a blank file
+        # file name formatting
+        file_name = f"{folder}/primes-{num_1_dec}{num_1_exp} \
+            -{num_2_dec}{num_2_exp}.txt"
+        file_list.append(file_name) # file name formatting
+        # creates a blank file
+        f = open(file_name,'w+'); f.close()
     return file_list
 
 # Driver code
